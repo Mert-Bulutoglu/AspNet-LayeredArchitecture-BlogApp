01ee750c13e2035ac0bf432e81801f6f545758d0
Mert Bulutoglu <mertbulutoglu95@gmail.com>
2022-06-17 16:11:48 +0300

    Personel Blog App with Admin Panel
    Combining pages with admin panel, editor panel and user panel.

---
+++ ProgrammersBlogewq/ProgrammersBlog.Mvc/Areas/Admin/Models/DashboardViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Entities.Dtos;
+using ProgrammersBlog.Services.Utilities;
+
+namespace ProgrammersBlog.Mvc.Areas.Admin.Models
+{
+    public class DashboardViewModel
+    {
+        public int CategoriesCount { get; set; }
+        public int ArticlesCount { get; set; }
+        public int CommentsCount { get; set; }
+        public int UserCount { get; set; }
+        public ArticleListDto Articles { get; set; }
+    }
+}
